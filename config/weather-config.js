d49201d60a545bcc37239b2c012474a77f24c5cf
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-04-21 13:10:01 +0200

    Create fetchSevenDaysWeather functions
    Also removed if..else from displayCurrentWeather

--- config/weather-config.js
+++ config/weather-config.js
@@ -1,4 +1,5 @@
 import { countries, weatherIcons } from "../helpers.js";
+import { geoLocation } from "./geolocation-config.js";
 
 const input = document.querySelector(".city-input");
 const heading = document.querySelector("h1");
@@ -11,51 +12,62 @@ const wind = document.querySelector("#wind");
 const weatherIcon = document.querySelector("#weather-icon");
 
 export const weather = {
-  apiKey: "4741d22028b090101d3735b88548e200",
-  fetchWeather: async (city) => {
-    try {
-      const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather.apiKey}`)
-      if(response.status===404) {
-        return alert("Can't find city name in database");
-      } else {
-        const data = await response.json();
-      return weather.displayWeather(data);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-},
-  displayWeather: (data) => {
-    const {
-      name,
-      wind: { speed },
-      main: { humidity, temp, temp_min, temp_max },
-      sys: { country },
-      weather: {
-        [0]: { description },
-      },
-    } = data;
-    heading.innerText = `${name},`;
-    countryName.innerText = `${countries[country]}`;
+    apiKey: "4741d22028b090101d3735b88548e200",
 
-    if (window.innerWidth < 465) {
-      temperature.innerText = `${parseInt(temp)}\xB0C`;
-      minTemp.innerText = `${temp_min}\xB0C`;
-      maxTemp.innerText = `${temp_max}\xB0C`;
-      hum.innerText = `${humidity}%`;
-      wind.innerText = `${speed}km/h`;
-      weatherIcon.src = weatherIcons[description];
-    } else { 
-      temperature.innerText = `${temp}\xB0C`;
-      minTemp.innerText = `Min: ${temp_min}\xB0C`;
-      maxTemp.innerText = `Max: ${temp_max}\xB0C`;
-      hum.innerText = `Humidity: ${humidity}%`;
-      wind.innerText = `Wind: ${speed}km/h`;
-      weatherIcon.src = weatherIcons[description];   
-    }
-  },
-  searchWeather: (value) => {
-    input.value = '';
-    weather.fetchWeather(value);
-  },
+    fetchCurrentWeather: async (city) => {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather.apiKey}`
+            );
+            if (response.status === 404) {
+                return alert("Can't find city name in database");
+            } else {
+                const data = await response.json();
+                return weather.displayCurrentWeather(data);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    },
+
+    fetchSevenDaysWeather: async (city) => {
+        const coords = await geoLocation.fetchCoords(city);
+
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${weather.apiKey}`
+            );
+            const data = await response.json();
+            return weather.displaySevenDaysWeather(data);
+        } catch (err) {
+            console.error(err);
+        }
+    },
+
+    displayCurrentWeather: (data) => {
+        const {
+            name,
+            wind: { speed },
+            main: { humidity, temp, temp_min, temp_max },
+            sys: { country },
+            weather: {
+                [0]: { description },
+            },
+        } = data;
+        heading.innerText = `${name},`;
+        countryName.innerText = `${countries[country]}`;
+
+        temperature.innerText = `${temp}\xB0C`;
+        minTemp.innerText = `Min: ${temp_min}\xB0C`;
+        maxTemp.innerText = `Max: ${temp_max}\xB0C`;
+        hum.innerText = `Humidity: ${humidity}%`;
+        wind.innerText = `Wind: ${speed}km/h`;
+        weatherIcon.src = weatherIcons[description];
+    },
+
+    searchWeather: (value) => {
+        weather.fetchCurrentWeather(value);
+        weather.fetchSevenDaysWeather(value);
+        input.value='';
+    },
 };
