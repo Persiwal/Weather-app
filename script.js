738c0bec2a0495323e2dd2227f1988efe51e9309
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-04-25 14:06:18 +0200

    Clean up

--- script.js
+++ script.js
@@ -5,46 +5,44 @@ import { cities } from "./config/cities-config.js";
 const suggestions = document.querySelector(".search-box__suggestions");
 const searchButton = document.querySelector(".search-button");
 const input = document.querySelector(".city-input");
-const cityHeader = document.querySelector('h1');
+const cityHeader = document.querySelector("h1");
 
 //display this weather on first page load
-  //unsplash.fetchPhoto();
-  weather.searchWeather('Warsaw');
-
+//unsplash.fetchPhoto();
+weather.searchWeather("Warsaw");
 
 // search by clicking button
 searchButton.addEventListener("click", () => {
-  if (input.value) {
-    //unsplash.fetchPhoto();
-    weather.searchWeather(input.value);
-    input.value = "";
-  }
+    if (input.value) {
+        //unsplash.fetchPhoto();
+        weather.searchWeather(input.value);
+        input.value = "";
+    }
 });
 
 // search by enter key
 input.addEventListener("keyup", (event) => {
-  if(input.value.length > 2) {
-    input.style.borderRadius = `0rem`;
-  } else {
-    input.style.borderRadius = `1.25rem`;
-  }
-  if (event.key === "Enter" && input.value) {
-   // unsplash.fetchPhoto();
-    weather.searchWeather(input.value);
-    input.value = "";
-  }
-})
-
-//suggestions
-input.addEventListener('keyup', () => {
-  if(input.value.length > 2) {
-    suggestions.classList.add('active');
-    cities.fetchCities(input.value);
- } else {
-   suggestions.classList.remove('active');
- }
+    if (input.value.length > 2) {
+        input.style.borderRadius = `0rem`;
+    } else {
+        input.style.borderRadius = `1.25rem`;
+    }
+    if (event.key === "Enter" && input.value) {
+        // unsplash.fetchPhoto();
+        weather.searchWeather(input.value);
+        input.value = "";
+    }
 });
 
+//suggestions
+input.addEventListener("keyup", () => {
+    if (input.value.length > 2) {
+        suggestions.classList.add("active");
+        cities.fetchCities(input.value);
+    } else {
+        suggestions.classList.remove("active");
+    }
+});
 
 // window.addEventListener('resize',() => {
 //   if(window.innerWidth<=465) {
